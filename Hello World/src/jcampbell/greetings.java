b4c051fde7f6bd08d97bc3bc2794f534735e7588
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-12 13:03:46 -0400

    My first push git hub push

---
+++ Hello World/src/jcampbell/greetings.java
@@ -0,0 +1,13 @@
+package jcampbell;
+
+public class greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String Javaughn = args[0];
+			String Campbell = args[1];
+			System.out.println("Hello " + Javaughn + " " + Campbell); System.out.println("Congratulations on your second program!");
+		}
+	}
+
+
