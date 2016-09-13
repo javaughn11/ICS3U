eba2568ef165687a2deb25f4666749368413acd0
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-13 13:39:00 -0400

    My About Me Exercise Created on September !3.2016 Its completed

---
+++ Hello World/src/jcampbell/AboutMe.java
@@ -0,0 +1,17 @@
+/* 
+ * About Me Exercise Created on September 13,2016
+ * By Javaughn Campbell
+ */
+package jcampbell;
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Javaughn"+" C");
+		System.out.println("Christina"+"Kemp");
+		System.out.println("RunnyMede"+" C.I");
+		System.out.println("Get"+" Money"+" Ravens");
+	}
+
+}
