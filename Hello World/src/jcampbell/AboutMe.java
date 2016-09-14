01e0838a7dac0af1f141943fb24d296a95de56a7
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-14 13:10:26 -0400

    Added The schedule which is the second part of the exercise I finished both Part 1 and Part 2 This is now fully completed

--- Hello World/src/jcampbell/AboutMe.java
+++ Hello World/src/jcampbell/AboutMe.java
@@ -1,17 +1,31 @@
 /* 
- * About Me Exercise Created on September 13,2016
+ * About Me Exercise  
+ * Created on September 13,2016 
  * By Javaughn Campbell
+ * Included my current schedule on Spetember 14, 2016
  */
 package jcampbell;
 
 public class AboutMe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Output Information
 		System.out.println("Javaughn"+" C");
 		System.out.println("Christina"+"Kemp");
 		System.out.println("RunnyMede"+" C.I");
 		System.out.println("Get"+" Money"+" Ravens");
+		// Output Current schedule
+		System.out.println( );
+		System.out.format("%-15s %10s %10s","Class ","Period ","Time");
+		System.out.println( );
+		System.out.format("\n%-15s %4s %22s","Parenting","1","8:45-10:02");
+		System.out.format("\n%-15s %4s %23s","English","2","10:07-11:24");
+		System.out.format("\n%-15s %3s %22s","Computer Science","3","12:26-1:43");
+		System.out.format("\n%-15s %4s %21s","Gym","4","1:48-3:05");
+
+
+		
 	}
+	
 
 }
