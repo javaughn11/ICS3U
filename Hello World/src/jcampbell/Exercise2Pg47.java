fc00464ed1ecfdb5b107bc57da4b9c482c8e3034
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-14 13:43:06 -0400

    Completed Exercise 2 on page 47.

---
+++ Hello World/src/jcampbell/Exercise2Pg47.java
@@ -0,0 +1,28 @@
+/*
+ * Exercise 2 on Page 47
+ * Created on September 14, 2016
+ * Made by Javaughn Campbell
+ */
+package jcampbell;
+
+public class Exercise2Pg47 {
+
+	public static void main(String[] args) {
+		// Output Bingo Game instructions
+		
+		System.out.println("Bingo Card");
+		System.out.println( );
+		System.out.println("1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println( );
+		System.out.println("2. The Number is placed on the bingo board and called out");
+		System.out.println( );
+		System.out.println("3. Players look for the calles number on their bingo card.");
+		System.out.println( );
+		System.out.println("4. If the number is located, it is marked off.");
+		System.out.println( );
+		System.out.println("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.");
+		System.out.println( );
+		System.out.println("6. The winning player yells BINGO.");
+	}
+
+}
