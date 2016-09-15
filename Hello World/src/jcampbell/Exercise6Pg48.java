286c8d2aa6d8903d56bcbd2b3aa4c6ab16110308
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-15 13:40:32 -0400

    Exercise 6 on page 48 is completed

---
+++ Hello World/src/jcampbell/Exercise6Pg48.java
@@ -0,0 +1,31 @@
+/*
+ * Exercise 6 on page 48
+ * Created on September 15, 2016
+ * Made by Javaughn Campbell
+ */
+package jcampbell;
+
+public class Exercise6Pg48 {
+
+	public static void main(String[] args) {
+		// Output TicTacToe Rules
+		
+		System.out.println("Tic Tac Tow Rules");
+		System.out.println( );
+		System.out.println("Tic Tac Toe is 2 player game");
+		System.out.println( );
+		System.out.println("1. Draw a grid with 9 squares (similar to a hashtag");
+		System.out.println( );
+		System.out.println("2. Player 1 draws an X in an empty space on the grid");
+		System.out.println( );
+		System.out.println("3. Player 2 draws an O in an empty space on the grid");
+		System.out.println( );
+		System.out.println("4. Steps 2 and 3 are repeated until a Tic Tac Pattern is created");
+		System.out.println( );
+		System.out.println("	Patterns can be diagonal, horizontal, or vertical.");
+		System.out.println( );
+		System.out.println("5. A winning pattern is completed by drawing a line through the pattern");
+		
+	}
+
+}
