2720b72b6f422af87156636d1939a34d27ca1b8b
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-15 13:29:23 -0400

    Exercise 6 on page 48 is completed

---
+++ Hello World/src/jcampbell/Exercise5Pg48.java
@@ -0,0 +1,27 @@
+/*
+ * Exercise 5 on page 47
+ * Created on September 15,2016
+ * Made by Javaughn Campbell
+ */
+package jcampbell;
+
+public class Exercise5Pg48 {
+
+	public static void main(String[] args) {
+		// Output TicTacToe board
+		
+		System.out.format("%8s %8s","|","|");
+		System.out.format("\n%8s %8s","|","|");
+		System.out.format("\n%8s %8s","|","|");
+		System.out.println("\n------------------------");
+		System.out.format("%8s %8s","|","|");
+		System.out.format("\n%8s %4s %3s","|","X","|");
+		System.out.format("\n%8s %8s","|","|");
+		System.out.println("\n------------------------");
+		System.out.format("%8s %8s","|","|");
+		System.out.format("\n%8s %8s","|","|");
+		System.out.format("\n%8s %8s","|","|");
+
+	}
+
+}
