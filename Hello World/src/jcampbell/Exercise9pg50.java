751a2c643274c8cc991611c19ac38ba8d485d03e
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-09-21 11:34:02 -0400

    Exercise 9 on page 50 is completed

---
+++ Hello World/src/jcampbell/Exercise9pg50.java
@@ -0,0 +1,37 @@
+/*
+ *Binary Numbers Exercise 9 on page 50
+ * Created on September 19,2016
+ * Made by Javaughn Campbell
+ */
+package jcampbell;
+
+public class Exercise9pg50 {
+
+	public static void main(String[] args) {
+		// Output Binary Numbers
+	
+		System.out.format("%1s %12s","Decimal","Binary\n");
+		System.out.format("%7s %12s","1","0\n");
+		System.out.format("%7s %12s","2","1\n");
+		System.out.format("%7s %12s","3","10\n");
+		System.out.format("%7s %12s","4","11\n");
+		System.out.format("%7s %12s","5","100\n");
+		System.out.format("%7s %12s","6","101\n");
+		System.out.format("%7s %12s","7","110\n");
+		System.out.format("%7s %12s","8","111\n");
+		System.out.format("%7s %12s","9","1000\n");
+		System.out.format("%7s %12s","10","1001\n");
+		System.out.format("%7s %12s","11","1010\n");
+		System.out.format("%7s %12s","12","1011\n");
+		System.out.format("%7s %12s","13","1100\n");
+		System.out.format("%7s %12s","14","1101\n");
+		System.out.format("%7s %12s","15","1110\n");
+		System.out.format("%7s %12s","16","1111\n");
+		System.out.format("%7s %12s","17","10000\n");
+		System.out.format("%7s %12s","18","10001\n");
+		System.out.format("%7s %12s","19","10010\n");
+		System.out.format("%7s %12s","20","10011\n");
+		
+	}
+
+}
