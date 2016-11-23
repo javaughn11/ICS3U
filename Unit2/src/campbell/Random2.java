fba33b9df625e719a562c9e9887b32799b44d30c
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2016-11-23 13:45:56 -0500

    added the while statement

--- Unit2/src/campbell/Random2.java
+++ Unit2/src/campbell/Random2.java
@@ -7,17 +7,26 @@
  */ 
 package campbell;
 
+import java.util.Scanner;
+
 public class Random2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);	
+		
 		int die1;
 		int die2;
 		int scoreuser;
 		int scorecpu;
+		String question;
+	
+		System.out.print("Would you like to play? ");
+		question = scan.nextLine();
 		
 		
+	do{	
 		die1 = (int)(Math.random()*6+1);
 		die2 = (int)(Math.random()*6+1);
 		scoreuser = die1 + die2;
@@ -26,19 +35,22 @@ public class Random2 {
 		die2 = (int)(Math.random()*6+1);
 		scorecpu = die1 + die2;
 		
-		System.out.println("your score is: " + scoreuser);
+		System.out.println("\nyour score is: " + scoreuser);
 		System.out.println("The computers score: " + scorecpu);
+	
 		
 	if (scoreuser == scorecpu)
 		System.out.println("It's a tie");
 	
 	else if (scoreuser > scorecpu)
-	      System.out.println("YOU WON!");
+	      System.out.println("YOU WON!\n");
 	
 	else
-		  System.out.println("The computer won, you lost");
+		  System.out.println("The computer won, you lost\n");
+	      System.out.print("Would you like to play again? ");
+	      question = scan.nextLine();
 	
-		
+	}while (question. equals ("yes"));	
 		
 		
 	}
