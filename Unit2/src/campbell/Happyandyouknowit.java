26ccddceac9a664f30ebd0450216a5668c3a00f1
Javaughn Campbell <Javaughn.Campbell@student.tdsb.on.ca>
2017-01-11 13:17:53 -0500

    Happy and you know it song

---
+++ Unit2/src/campbell/Happyandyouknowit.java
@@ -0,0 +1,33 @@
+package campbell;
+
+/*Happyandyouknowit.java
+ *Made by Javaughn Campbell
+ *Created on Monday, January 9, 2017
+ *program displays that sings the happy song
+ */
+
+
+public class Happyandyouknowit {
+
+
+	public static void main(String[] args) {
+
+		String [] verses = new String [] {"clap your hands", "stomp your feet", "jump around", "run around", "say la la la", "put your left hand up", "put your right hand up","put your left foot in", "put your right foot in", "touch your head"}; 
+
+		for (int i=0; i<10; i++)
+
+		{
+
+			System.out.println("If your happy and you know it " + verses[i]);
+			System.out.println("If your happy and you know it " + verses[i]);
+			System.out.println("If your happy and you know it and you really want to show it ");
+			System.out.println("If your happy and you know it " + verses[i]);
+			System.out.println(" ");
+
+		}
+
+	}
+
+
+
+}
